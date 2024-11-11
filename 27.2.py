69f480ebd203c42240414b92f0e92a151cd3b1d0
aliumasev <aliumasev17@gmail.com>
2024-11-11 16:20:42 +0300

    v1

--- 27.2.py
+++ 27.2.py
@@ -1,7 +1,17 @@
+while True:
+    try:
+        n=int(input("Введите целое число n="))
+    except:
+        print("Это не целое число,попробуй еще раз!")
+    else:
+        if n<=1:
+            print("Это целое число не больше 1,попробуй еще раз!")
+        else:
+            break
 M = 80
 m1 = 400
 m2 = range(100, 320, 20)
 
 for m2 in m2:
-    a = (m1 - m2)/(M + m1 + m2)
-    print(f"Для m2 = {m2} г. ускорение равно {a}")
+    a = m2- m1/m1+ m2 + M/2 *
+    print(a)
--- 27.3.py
+++ 27.3.py
@@ -1,6 +1,17 @@
-N = int(input('Введите целое число N: '))
+while True:
+    try:
+        N=int(input("Введите целое число n="))
+    except:
+        print("Это не целое число,попробуй еще раз!")
+    else:
+        if n<=1:
+            print("Это целое число не больше 1,попробуй еще раз!")
+        else:
+            break
 k = 0
 
 for i in range(1, N+1):
     k += 2*i - 1
     print('Квадрат числа', i, ':', k)
+
+    

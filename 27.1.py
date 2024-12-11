a735883e1c7714390d6735c777683dfdd1dd2930
aliumasev <aliumasev17@gmail.com>
2024-12-11 09:21:42 +0300

    v1

---
+++ 27.1.py
@@ -0,0 +1,26 @@
+while True:
+    try:
+        m = int(input('Введите число М - '))
+        if m<0:
+            print('Это число меньше 0,попробуйте еще раз')
+        else:
+            break
+    except:
+        print('Ошибка,попробуйте еще раз')
+import random
+A=[]
+for i in range(m):
+    n=[]
+    for j in range(m):
+        n.append(random.randint(-10,10))
+    A.append(n)
+print(A)
+k = 0
+for i in range(1,m+1):
+    for j in range(1,m+1):
+        if ((i+j)>(m+1)) and A[i-1][j-1]<0:
+            k+=1
+print('Количество отрицательных элементов ниже побочной диагонали -',k)
+
+
+

bd1c373c7c0ef1abcd97ed5e8eee7c7ab955a1b4
Али Умашев <aliumasev17@gmail.com>
2025-02-13 13:40:54 +0000

    add implementation for calculating z based on user input

---
+++ map.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <>
+int main() {
+    double x, y, z;
+
+    std::cout << "Введите значение x: ";
+    std::cin >> x;
+    std::cout << "Введите значение y: ";
+    std::cin >> y;
+
+    z = x * x * x + 2 * x * x - 5 * y;
+
+    std::cout << "Значение z: " << z << std::endl;
+
+    return 0;
+}

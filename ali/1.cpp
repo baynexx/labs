d66f5f516611977a2613bd47164eec66ed516670
Али Умашев <aliumasev17@gmail.com>
2025-02-13 18:37:58 +0000

    добавить новую программу для вычисления y на основе пользовательского ввода и удалить устаревшую версию

---
+++ ali/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int x;
+    int y;
+    cout << "Введите x: ";
+    cin >> x;
+    if (x < -2) {
+        y = (sin(6*pow(x, 3)))/ (4*x-pow(x,4));
+    } else if (-2 <= x < 3) {
+        y = (2*log(x-1) - pow(exp(1), x))/(8*x + pow(x, 2));
+    } else if (x>=3){
+        y = (2*pow(x, 3) + 4)/((5*pow(x, 2) - 2) * tan(2*x + 1));
+    }
+    cout << y;
+    return 0;
+}

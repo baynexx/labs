ca1e92f3e4aa41cd655a43e20aadb24644434eec
Али Умашев <aliumasev17@gmail.com>
2025-02-14 06:32:52 +0000

    1

--- ali/1.cpp
+++ ali/1.cpp
@@ -13,7 +13,10 @@ int main()
     if (x < -2) {
         y = (sin(6*pow(x, 3)))/ (4*x-pow(x,4));
     } else if (-2 <= x < 3) {
-        y = (2*log(x-1) - pow(exp(1), x))/(8*x + pow(x, 2));
+        if (x !=0)
+            y = (2*log(x-1) - pow(exp(1), x))/(8*x + pow(x, 2));
+        else
+            cout << "x = 0 ";
     } else if (x>=3){
         y = (2*pow(x, 3) + 4)/((5*pow(x, 2) - 2) * tan(2*x + 1));
     }

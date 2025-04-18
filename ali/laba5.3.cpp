ae47244f6477bbb3a4bcd8f28eb7dd426f09e516
Али Умашев <aliumasev17@gmail.com>
2025-04-18 06:01:25 +0000

    v1

--- ali/laba5.3.cpp
+++ ali/laba5.3.cpp
@@ -1,31 +1,46 @@
 #include <iostream>
-#include <cmath>
 #include <limits>
 
 using namespace std;
 
 void pow_2(unsigned n)
 {
-	if (n > 2)
-		if (n % 2 == 1)
-			cout << "NO";
-		else
-			pow_2(n / 2);
-	else if (n == 1)
-		cout << "NO";
-	else 
-		cout << "YES";
+    if (n == 0) {
+        cout << "NO";
+        return;
+    }
+    
+    if (n == 1) {
+        cout << "NO";
+        return;
+    }
+    
+    if (n == 2) {
+        cout << "YES";
+        return;
+    }
+    
+    if (n % 2 == 1) {
+        cout << "NO";
+        return;
+    }
+    
+    pow_2(n / 2);
 }
 
 int main() {
-	setlocale(0, "rus");
-	unsigned n;
-	while (!(cin >> n))
-	{
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cout << "Неверный ввод! Введите натуральное число: ";
-		cin >> n;
-	}
-	pow_2(n);
-}	
+    setlocale(0, "rus");
+    unsigned n;
+    
+    cout << "Введите натуральное число: ";
+    while (!(cin >> n))
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Неверный ввод! Введите натуральное число: ";
+    }
+    
+    pow_2(n);
+    
+    return 0;
+}
